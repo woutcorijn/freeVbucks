77a94cdaa061e036fc17f573c3c5668a005ceb93
woutcorijn <wout.corijn@hotmail.com>
2018-03-25 13:06:17 +0200

    V1.03

--- js.js
+++ js.js
@@ -1,5 +1,6 @@
 console.log("this is fake");
 var click = 0;
+var bonus = 500;
 
 function press() {
 	click = click + 1;
@@ -11,4 +12,11 @@ function get() {
 	if (click > 5000) {
 		window.open("fake.html");
 	}
+}
+
+function share() {
+	click = click + bonus;
+	document.getElementById("am").innerHTML = "Amount: " + click;
+	bonus = 0;
+	window.open("share.html");
 }
